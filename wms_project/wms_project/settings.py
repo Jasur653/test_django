e8eefb763268fee35954418a2558c5c89924e995
Jasur653 <jasurtolqinov113@gmail.com>
2025-06-12 15:01:38 +0500

    Update settings.py

--- wms_project/wms_project/settings.py
+++ wms_project/wms_project/settings.py
@@ -8,13 +8,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = 'django-insecure-your-secret-key-here'
-
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-your-secret-key-here')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'test-django-saqz.onrender.com').split(',')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+CSRF_TRUSTED_ORIGINS = ['https://test-django-saqz.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
